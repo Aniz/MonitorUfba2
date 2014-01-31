fe6a2e7cca22e504fdf8377868b30bc6fcfceb5f
Arlindo Jr <ba.elias@gmail.com>
2014-01-31 09:57:56 -0300

    comeco login

---
+++ controller/login.php
@@ -0,0 +1,26 @@
+<?php 
+require_once '../vendor/autoload.php';
+require_once '../helper/twig.php';
+require_once('../helper/funcoes.php');
+
+session_start(); 
+
+$login = $_POST['login']; 
+$senha = $_POST['senha']; 
+
+
+$con = mysql_connect("127.0.0.1", "root", "") or die ("Sem conexão com o servidor");
+$select = mysql_select_db("monitoria") or die("Sem acesso ao DB, Entre em contato com os Administradores Arlindo, Anna ou João vitor"); // A vriavel $result pega as varias $login e $senha, faz uma pesquisa na tabela de usuarios 
+
+$result = mysql_query("SELECT * FROM professor WHERE email = '$login' AND SENHA = '$senha'"); 
+if(mysql_num_rows ($result) > 0 ) { 
+	$_SESSION['login'] = $login; 
+	$_SESSION['senha'] = $senha; 
+	header('location:./index.php'); 
+}
+else{ 
+	unset ($_SESSION['login']); 
+	unset ($_SESSION['senha']); 
+	
+	header('location:/login.php'); 
+} ?>
